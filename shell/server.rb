dc778f9a50be0ececf35ddb9962535bb5fd4cb62
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-08-04 15:41:58 -0400

    First commit. Started work on shell exploitable sinatra app in ./shell.

---
+++ shell/server.rb
@@ -0,0 +1,65 @@
+# sinatra app
+# use sqlite
+# guestbook
+# -> backtick to a markdown parser
+require 'date'
+require 'sinatra'
+require 'pry'
+
+class Guestbook
+  attr_accessor :comments
+  def initialize
+    @comments = []
+  end
+  def markdownify(string)
+    `./markdown.sh #{string}`
+  end
+
+  def comment(name, string)
+    # comments << {user: name, comment: markdownify(string), timestamp: DateTime.now}
+    comments << {name: name, comment: string, timestamp: DateTime.now}
+  end
+
+end
+
+guestbook = Guestbook.new
+
+get '/' do
+  binding.pry
+  erb :index, {locals: {comments: guestbook.comments}}
+end
+
+post '/guestbook/comments' do
+  guestbook.comment(params[:name], params[:comment])
+  redirect '/'
+end
+
+__END__
+
+@@ layout
+<html>
+    <head>
+        <title>My Wedding Guestbook</title>
+    </head>
+    <body>
+        <%= yield %>
+    </body>
+</html>
+
+@@ index
+<h1>My Wedding Guestbook</h1>
+I'm getting married. Leave a message about how much you love me!
+<form action='/guestbook/comments' method='POST'>
+    <label>Name: </label>
+    <input type='text' name='name'>
+    <label>Comment: </label>
+    <input type='text' name='comment'>
+    <input type='submit'>
+</form>
+            
+<% comments.each do |comment| %>
+  <div>
+   <p>Name: <%= comment[:name] %></p>
+   <p>Time: <%= comment[:timestamp] %> </p>
+   <p><%= comment[:comment] %>
+<% end %>
