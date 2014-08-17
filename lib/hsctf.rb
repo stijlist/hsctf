e6a3ae08bce62e5c464c6e750a638e5606490705
Max Veytsman <maxim@ontoillogical.com>
2014-08-17 16:11:14 -0400

    config file loaded in the wrong place

--- lib/hsctf.rb
+++ lib/hsctf.rb
@@ -2,3 +2,9 @@ require 'hsctf/game.rb'
 require 'hsctf/ctf.rb'
 require 'hsctf/quartermaster'
 require 'hsctf/messager'
+
+
+env_file = File.join(File.dirname(__FILE__), '../config', 'env.yml')
+YAML.load(File.open(env_file)).each do |key, value|
+  ENV[key.to_s] = value
+end if File.exists?(env_file)
