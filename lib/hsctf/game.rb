f37f723e1900431d17cd5a3bc724a083b885c414
Max Veytsman <maxim@ontoillogical.com>
2014-08-27 13:39:06 -0400

    Fixed bug with scoring

--- lib/hsctf/game.rb
+++ lib/hsctf/game.rb
@@ -110,8 +110,8 @@ end
 
 class Leaders
   def self.given(players)
-    sorted_by_score = players.sort_by{|p| p['score']}.reverse
-    highest_score = sorted_by_score.first['score']
-    sorted_by_score.take_while {|p| p['score'] == highest_score }
+    sorted_by_score = players.sort_by{|p| p[:score]}.reverse
+    highest_score = sorted_by_score.first[:score]
+    sorted_by_score.take_while {|p| p[:score] == highest_score }
   end
 end
